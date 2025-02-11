97a7206269c40f64f18b9b560cc66c2d98e87b79
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-02-11 09:55:12 -0300

    feat: run project

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const app = require("./config/config.js")
+
+require("./routes.js")
+
+app.listen(3000, (err) => {
+  if (err) throw err
+  console.log("Server is running on port 3000")
+})
